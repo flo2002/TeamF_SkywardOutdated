9e68afa9178adf7f46b0e64fa4247addb3e1ceea
xJustCallMeAlex <alexander.kolar1379@gmail.com>
2022-11-02 22:30:44 +0100

    finishing some controller

--- src/main/java/fhv/ws22/se/skyward/view/BookingController.java
+++ src/main/java/fhv/ws22/se/skyward/view/BookingController.java
@@ -1,6 +1,16 @@
 package fhv.ws22.se.skyward.view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class BookingController {
     public BookingController() {
@@ -8,7 +18,18 @@ public class BookingController {
     }
 
     @FXML
-    public void onCreateBookingButtonClick() {
+    public void onCreateBookingButtonClick(ActionEvent event) {
         System.out.println("Let's create a Booking!");
+        try {
+            URL url = new File("src/main/resources/fhv/ws22/se/skyward/create-bookings.fxml").toURI().toURL();
+            Parent parent = FXMLLoader.load(url);
+
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setTitle("Home");
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/fhv/ws22/se/skyward/view/CreateBookingController.java
+++ src/main/java/fhv/ws22/se/skyward/view/CreateBookingController.java
@@ -1,6 +1,16 @@
 package fhv.ws22.se.skyward.view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class CreateBookingController {
     public CreateBookingController() {
@@ -13,7 +23,17 @@ public class CreateBookingController {
     }
 
     @FXML
-    public void onReturnToBookingsButtonClick() {
+    public void onReturnToBookingsButtonClick(ActionEvent event) {
+        try {
+            URL url = new File("src/main/resources/fhv/ws22/se/skyward/bookings.fxml").toURI().toURL();
+            Parent parent = FXMLLoader.load(url);
 
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setTitle("Home");
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
