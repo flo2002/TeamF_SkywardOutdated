e597acf3c0a63f442f48dac236b3dfbf5727d1c0
flo2002 <florian.schiemer003@gmail.com>
2022-11-11 10:11:28 +0100

    addGuest

--- src/main/java/fhv/ws22/se/skyward/view/AddGuestController.java
+++ src/main/java/fhv/ws22/se/skyward/view/AddGuestController.java
@@ -1,16 +1,49 @@
 package fhv.ws22.se.skyward.view;
 
+import fhv.ws22.se.skyward.domain.Session;
+import fhv.ws22.se.skyward.domain.SessionFactory;
+import fhv.ws22.se.skyward.domain.dtos.BookingDto;
+import fhv.ws22.se.skyward.domain.dtos.CustomerDto;
 import fhv.ws22.se.skyward.view.util.ControllerNavigationUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddGuestController {
     private static final Logger logger = LogManager.getLogger("AddGuestController");
+    private static final BigInteger clientSessionID = new BigInteger("1");
+    private Session session;
+
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private void initialize() {
+        session = SessionFactory.getInstance().getSession(clientSessionID);
+    }
 
     @FXML
     public void onConfirmButtonClick(ActionEvent event) {
+        if (firstNameTextField.getText().isEmpty()) {
+            firstNameTextField.setStyle("-fx-border-color: red ; -fx-border-width: 1px ;");
+            return;
+        }
+        if (lastNameTextField.getText().isEmpty()) {
+            lastNameTextField.setStyle("-fx-border-color: red ; -fx-border-width: 1px ;");
+            return;
+        }
+
+        CustomerDto customerDto = new CustomerDto(firstNameTextField.getText(), lastNameTextField.getText());
+        session.add(customerDto);
+
         ControllerNavigationUtil.navigate(event, "src/main/resources/fhv/ws22/se/skyward/bookings.fxml", "Booking");
     }
 
@@ -21,7 +54,6 @@ public class AddGuestController {
 
     @FXML
     public void onHomeButtonClick(ActionEvent event) {
-        System.out.println("This works, right?");
         ControllerNavigationUtil.navigate(event, "src/main/resources/fhv/ws22/se/skyward/homescreen.fxml", "Home");
     }
 
