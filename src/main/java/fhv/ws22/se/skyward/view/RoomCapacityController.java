05d46698e0243a6f57f968603eec3ab2a644ea56
flo2002 <florian.schiemer003@gmail.com>
2022-12-01 19:58:02 +0100

    Update RoomCapacityController.java

--- src/main/java/fhv/ws22/se/skyward/view/RoomCapacityController.java
+++ src/main/java/fhv/ws22/se/skyward/view/RoomCapacityController.java
@@ -2,6 +2,8 @@ package fhv.ws22.se.skyward.view;
 
 import fhv.ws22.se.skyward.domain.dtos.BookingDto;
 import fhv.ws22.se.skyward.domain.dtos.RoomDto;
+import fhv.ws22.se.skyward.domain.model.BookingModel;
+import fhv.ws22.se.skyward.domain.model.RoomModel;
 import fhv.ws22.se.skyward.persistence.entity.Room;
 import fhv.ws22.se.skyward.view.util.RoomCapacity;
 import javafx.beans.property.SimpleObjectProperty;
@@ -21,26 +23,39 @@ public class RoomCapacityController extends AbstractController {
     @FXML
     private TableView<RoomCapacity> table;
 
-    private Boolean isOccupied(List<BookingDto> bookings, RoomDto room, LocalDate date) {
-        //session.getAvailableRooms(date, date);
+    private Boolean isOccupied(LocalDate date) {
+        List<BookingDto> bookings = (List<BookingDto>) session.getAll(BookingDto.class);
+        // check if any booking is in the same time frame to remove it from the available rooms
+        for (BookingDto booking : bookings) {
+            //if (booking.getCheckInDateTime().toLocalDate().isBefore(date) || booking.getCheckOutDateTime().toLocalDate().isAfter(date)) {
+            if (date.isBefore(booking.getCheckOutDateTime().toLocalDate()) && date.isAfter(booking.getCheckInDateTime().toLocalDate())) {
+                List<RoomDto> blockedRooms = booking.getRooms();
+                if (blockedRooms != null) {
+                    for (RoomDto room : blockedRooms) {
+                        if (room.getRoomNumber().equals(room.getRoomNumber())) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
     @FXML
     protected void initialize() {
         List<RoomDto> rooms = session.getAll(RoomDto.class);
-        List<BookingDto> bookings = session.getAll(BookingDto.class);
         List<RoomCapacity> roomCaps = new ArrayList<>();
 
         for (RoomDto room : rooms) {
             RoomCapacity roomCap = new RoomCapacity();
 
             roomCap.setRoomNumber(room.getRoomNumber());
-            roomCap.setDay1(isOccupied(bookings, room, LocalDate.now()));
-            roomCap.setDay2(isOccupied(bookings, room, LocalDate.now().plusDays(1)));
-            roomCap.setDay3(isOccupied(bookings, room, LocalDate.now().plusDays(2)));
-            roomCap.setDay4(isOccupied(bookings, room, LocalDate.now().plusDays(3)));
-            roomCap.setDay5(isOccupied(bookings, room, LocalDate.now().plusDays(4)));
+            roomCap.setDay1(isOccupied(LocalDate.now()));
+            roomCap.setDay2(isOccupied(LocalDate.now().plusDays(1)));
+            roomCap.setDay3(isOccupied(LocalDate.now().plusDays(2)));
+            roomCap.setDay4(isOccupied(LocalDate.now().plusDays(3)));
+            roomCap.setDay5(isOccupied(LocalDate.now().plusDays(4)));
 
             roomCaps.add(roomCap);
         }
@@ -48,22 +63,14 @@ public class RoomCapacityController extends AbstractController {
         TableColumn<RoomCapacity, String> roomNumberCol = new TableColumn<>("Room Number");
         roomNumberCol.setCellValueFactory(new PropertyValueFactory<>("roomNumber"));
 
-        TableColumn<RoomCapacity, String> day1Col = new TableColumn<>("Day 1");
-        day1Col.setCellValueFactory(entry -> new SimpleObjectProperty<>(entry.getValue().getDay1() ? "occupied" : "free"));
+        List<TableColumn<RoomCapacity, String>> columns = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            TableColumn<RoomCapacity, String> column = new TableColumn<>(LocalDate.now().plusDays(i).format(DateTimeFormatter.ofPattern("dd.MM")));
+            column.setCellValueFactory(entry -> new SimpleObjectProperty<>(entry.getValue().getDay1() ? "occupied" : "free"));
+            columns.add(column);
+        }
 
-        TableColumn<RoomCapacity, Boolean> day2Col = new TableColumn<>("Day 2");
-        day2Col.setCellValueFactory(new PropertyValueFactory<>("day2"));
-
-        TableColumn<RoomCapacity, Boolean> day3Col = new TableColumn<>("Day 3");
-        day3Col.setCellValueFactory(new PropertyValueFactory<>("day3"));
-
-        TableColumn<RoomCapacity, Boolean> day4Col = new TableColumn<>("Day 4");
-        day4Col.setCellValueFactory(new PropertyValueFactory<>("day4"));
-
-        TableColumn<RoomCapacity, Boolean> day5Col = new TableColumn<>("Day 5");
-        day5Col.setCellValueFactory(new PropertyValueFactory<>("day5"));
-
-        table.getColumns().addAll(roomNumberCol, day1Col, day2Col, day3Col, day4Col, day5Col);
+        table.getColumns().addAll(roomNumberCol, columns.get(0), columns.get(1), columns.get(2), columns.get(3), columns.get(4), columns.get(5));
         table.getItems().addAll(roomCaps);
     }
 }
